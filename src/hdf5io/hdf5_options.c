374ecd37f9cf1fd99cd44a3aa85c9aa2d34b7aad
Pablo Oyola <oyola@pppl.gov>
2026-05-20 16:10:30 -0400

    We only need to set the pushing function when we are in the FO mode

--- src/hdf5io/hdf5_options.c
+++ src/hdf5io/hdf5_options.c
@@ -67,8 +67,8 @@ int hdf5_options_read(hid_t file, sim_data* sim, char* qid){
     
     // If we have that the user defined the full-orbit case, 
     // we will read the solver name and set the function pointer accordingly.
-    set_push_function(1); // This will set the function pointer to the default solver, which is VPA phase corrected. If the user has specified a different solver, this will be overwritten by the next lines.
     if(sim->sim_mode == 1) {
+        set_push_function(1); // This will set the function pointer to the default solver, which is VPA phase corrected. If the user has specified a different solver, this will be overwritten by the next lines.
         if( hdf5_read_double(OPTPATH "FULL_ORBIT_SOLVER", &tempfloat,
                              file, qid, __FILE__, __LINE__) ) {
             print_out0(VERBOSE_NORMAL, sim->mpi_rank, sim->mpi_root, \
